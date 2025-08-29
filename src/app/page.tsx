059afb4d6c0c7f0783d710d1d03fb40fce5e23e9
divyansh-prks <divyansh2375@gmail.com>
2025-08-29 19:50:08 +0530

    added client way of using trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+'use client'
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
 
 export default function Home() {
+  const trpc = useTRPC() ; 
+  const {data } = useQuery(trpc.hello.queryOptions({text : "divyansh"}))
+  trpc.hello.queryOptions({text : "Hello "})
   return (
  <div>
   <h1> Hello world </h1>
+  <div>
+    {JSON.stringify(data)}
+  </div>
  </div>
   );
 }
