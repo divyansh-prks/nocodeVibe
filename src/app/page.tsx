53cef8dcdff203a8185f6d6d2e18e56dcaacdb10
divyansh-prks <divyansh2375@gmail.com>
2025-08-30 15:16:39 +0530

    Agent responsing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,29 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 // import { Button } from "@/components/ui/button";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 import { toast } from "sonner";
 
 const HomePage = () => {
+
+  const [value , setValue] = useState("");
   const trpc = useTRPC();
   const invoke = useMutation(trpc.invoke.mutationOptions({
     onSuccess: () => {
       toast.success("Background Job Started Successfully!");
     }
   }))
+
+
   return (
     <div className="p-4 max-2-7xl mx-auto">
+      <Input value={value} onChange={(e) => setValue(e.target.value)}/>
       
-      
-      <Button disabled={invoke.isPending} onClick={() => invoke.mutate({ text: "divyansh" })}>Invoke Background Job</Button>
+      <Button disabled={invoke.isPending} onClick={() => invoke.mutate({ value : value })}>Invoke Background Job</Button>
     </div>
   )
 }
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,14 +1,44 @@
 import { inngest } from "./client";
+// import {    openai, createAgent } from "@inngest/agent-kit";
+import {openai , gemini, createAgent } from "@inngest/agent-kit";
+import { createNetwork } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "30s");
+  async ({ event}) => {
+    // await step.sleep("wait-a-moment", "10s");
 
-    // another wait 30 second for transcribing 
+    const model = gemini({
+  model: "gemini-1.5-flash",
+});
 
-    // another wait 10 sec for summarizing 
-    return { message: `Hello ${event.data.email}!` };
+
+//rough 
+// const supportAgent = createAgent({
+//   model: model,
+//   name: "Customer support specialist",
+//   system: "You are an customer support specialist... You can give some advise ",
+// });
+
+
+
+//fair 
+
+    const codeAgent= createAgent({
+  model: model,
+  name: "code-agent",
+  system: "You are an expert next js developer.  You write readable and maintainable code . Your write simple nextsjs and react js snippets  .",
+  
+});
+//       // Run the agent with an input.  This automatically uses steps
+//     // to call your AI model.
+    const { output } = await codeAgent.run(
+        `Summarize : ${event.data.value} `
+
+        );
+
+        console.log(output)
+    return {output};
   },
 );
